a8aac2fd6e5bb21bdcb527b057fca2c4cd191757
Minch <chenmchn@gmail.com>
2017-10-29 10:19:31 +0800

    tab

---
+++ js/main.js
@@ -0,0 +1,16 @@
+layui.define([
+    'element',
+    'jquery'
+], function(exports) {
+    var element = leyui.element(),$ = layui.jquery,layId,Tab=function(){
+        this.tabConfig = {
+            closed : true,
+            openTabNum : undefined,
+            tabFilter : "system_tab",
+            url : undefined
+        }
+    };
+
+    
+    
+});
